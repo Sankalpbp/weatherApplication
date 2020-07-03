a88ae03b86e52640c7a05195ea1ebc874fee7d09
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-03 15:30:12 +0530

    NPM initialized and an HTTP Request

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-console.log('Starting');
+'use strict'
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 seconds');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=1033c76d0c1530af36960575ab2b03a8&query=New%20York';
 
-setTimeout(() => {
-    console.log('0');
-}, 0);
+// request is a function
+// it will take two arguments
+// one : the url
+// and when it will fetch the data from the url, it will call the "Callback function", which is the second 
+// argument
 
-console.log('Stoppin');
+request({ url:url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+});
